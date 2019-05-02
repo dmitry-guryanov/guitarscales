91619db01e92722c2b5c355384d3365de8b731c1
Dmitry Guryanov <dmitry.guryanov@virtuozzo.com>
2019-05-03 02:03:30 +0300

    Improve menu

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import stateStore from "./stores"
 import {noteNameJs, notesTable} from "./models/Note"
 import {tunings, tuningIds} from "./models/Tuning"
 
+function remove(array, element) {
+  const index = array.indexOf(element);
+  array.splice(index, 1);
+}
+
 const App = observer(class App extends Component {
 
   handleSelectKeyNote(n) {
@@ -25,9 +30,35 @@ const App = observer(class App extends Component {
   }
 
   render() {
+    let scalesOrig = Scale.names()
+    let scales = [["major", "minor"],
+      ["major pentatonic", "minor pentatonic"],
+      ["ionian", "dorian", "phrygian", "lydian", "mixolydian", "aeolian", "locrian"],
+      ["chromatic"]]
+    for (let sl of scales) {
+      for (let s of sl) {
+        remove(scalesOrig, s)
+      }
+    }
+    scales.push(scalesOrig)
+    let cc = []
+    let first = true
+    for (let sl of scales) {
+      if (!first) {
+        cc.push((<NavDropdown.Divider />))
+      } else {
+        first = false
+      }
+      for (let s of sl) {
+          cc.push((<NavDropdown.Item key={s} eventKey={s}>{s}</NavDropdown.Item>))
+      }
+
+    }
+
+    
     return (
       <div className="App">
-        <Navbar bg="light" variant="light">
+        <Navbar size="sm" bg="light" variant="light">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
           <Nav onSelect={this.handleSelectKeyNote}>
@@ -49,27 +80,30 @@ const App = observer(class App extends Component {
           </Nav>
           <Nav onSelect={this.handleSelectScale}>
           <NavDropdown title={stateStore.scaleType} id="scale-drop-down">
-          {
-            Scale.names().map((id) =>
-              <NavDropdown.Item>
-                <Nav.Link key={id} eventKey={id}>{id}</Nav.Link>
-              </NavDropdown.Item>
-            )
-          }
+                {cc}
           </NavDropdown>
           </Nav>
           <Nav onSelect={this.handleSelectTuning}>
           <NavDropdown title={stateStore.tuning.name} id="scale-drop-down">
           {
                 tuningIds.map((id) =>
-              <Nav.Item>
-                <Nav.Link key={id} eventKey={id}>{tunings[id].name}</Nav.Link>
-              </Nav.Item>
+              <NavDropdown.Item key={id} eventKey={id}>{tunings[id].name}</NavDropdown.Item>
             )
           }
           </NavDropdown>
           </Nav>
           </Navbar.Collapse>
+
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <NavDropdown alignRight title="My other projects" id="basic-nav-dropdown">
+                <NavDropdown.Item href="http://ixshot.com">Intelligent photo studio IxShot</NavDropdown.Item>
+                <NavDropdown.Item href="https://dmitryguryanov.myportfolio.com/">My photo gallery</NavDropdown.Item>
+                <NavDropdown.Item href="http://guryanov.org/pickup-calculator/">Guitar pickup frequency response calculator</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+
         </Navbar>
         <br />
         <ScaleScheme />
